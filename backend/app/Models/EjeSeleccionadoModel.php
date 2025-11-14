93ec21f31cb02318a5a89036915a225533a8b26e
Nunu-DMG09 <davidmesta09@gmail.com>
2025-11-14 13:06:58 -0500

    Refactor EjeSeleccionadoModel to enable timestamps and add validation rules for fields

--- backend/app/Models/EjeSeleccionadoModel.php
+++ backend/app/Models/EjeSeleccionadoModel.php
@@ -8,6 +8,7 @@ class EjeSeleccionadoModel extends Model
     protected $table      = 'eje_seleccionado';
     protected $primaryKey = 'id';
     protected $returnType = 'array';
+
     protected $allowedFields = [
         'id_rendicion',
         'id_eje',
@@ -15,15 +16,34 @@ class EjeSeleccionadoModel extends Model
     ];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
-    protected $createdField  = '';
-    protected $updatedField  = '';
-    protected $deletedField  = '';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
+    protected $validationRules = [
+        'id_rendicion'      => 'required|integer',
+        'id_eje'            => 'required|integer',
+        'cantidad_pregunta' => 'required|integer'
+    ];
+
+    protected $validationMessages = [
+        'id_rendicion' => [
+            'required' => 'El campo {field} es obligatorio.',
+            'integer'  => 'El campo {field} debe ser un número entero.'
+        ],
+        'id_eje' => [
+            'required' => 'El campo {field} es obligatorio.',
+            'integer'  => 'El campo {field} debe ser un número entero.'
+        ],
+        'cantidad_pregunta' => [
+            'required' => 'El campo {field} es obligatorio.',
+            'integer'  => 'El campo {field} debe ser un número entero.'
+        ]
+    ];
+
     protected $skipValidation       = false;
     protected $cleanValidationRules = true;
 }
