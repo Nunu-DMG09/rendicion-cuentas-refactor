0d166b831ccb5121cb6b9e7d895bc93e4127799d
Nunu-DMG09 <davidmesta09@gmail.com>
2025-11-14 10:20:29 -0500

    Add HistorialAdminModel class for admin history data management

---
+++ backend/app/Models/HistorialAdminModel.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class HistorialAdminModel extends Model
+{
+    protected $table      = 'historial_admin';
+    protected $primaryKey = 'id';
+    protected $returnType = 'array';
+    protected $allowedFields = [
+        'id_admin',
+        'accion',
+        'motivo',
+        'realizado_por',
+        'fecha_accion'
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'fecha_accion';
+    protected $updatedField  = '';
+    protected $deletedField  = '';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+}
