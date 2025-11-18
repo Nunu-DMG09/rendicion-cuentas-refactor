22643c8a4f983c54df82edcb3566035ecfbf0ff0
Nunu-DMG09 <davidmesta09@gmail.com>
2025-11-17 22:00:54 -0500

    Refactor SQL dump to change character set from utf8mb4 to utf8 for compatibility

--- sql/rendicion_cuenta2.sql
+++ sql/rendicion_cuenta2.sql
@@ -15,13 +15,13 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
+-- 
 -- Table structure for table `administrador`
 --
 
 DROP TABLE IF EXISTS `administrador`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `administrador` (
   `id` int NOT NULL AUTO_INCREMENT,
   `dni` varchar(8) NOT NULL,
@@ -30,10 +30,10 @@ CREATE TABLE `administrador` (
   `categoria` enum('admin','super_admin') NOT NULL,
   `estado` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
+-- 
 -- Dumping data for table `administrador`
 --
 
@@ -43,23 +43,23 @@ INSERT INTO `administrador` VALUES (1,'40346175','MARTHA LUZ TUÑOQUE JULCAS','$
 /*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
+-- 
 -- Table structure for table `baner_rendicion`
 --
 
 DROP TABLE IF EXISTS `baner_rendicion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `baner_rendicion` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_rendicion` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_br_rendicion` (`id_rendicion`),
   CONSTRAINT `fk_br_rendicion` FOREIGN KEY (`id_rendicion`) REFERENCES `rendicion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
+-- 
 -- Dumping data for table `baner_rendicion`
 --
 
@@ -68,22 +68,22 @@ LOCK TABLES `baner_rendicion` WRITE;
 /*!40000 ALTER TABLE `baner_rendicion` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
+-- 
 -- Table structure for table `eje`
 --
 
 DROP TABLE IF EXISTS `eje`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `eje` (
   `id` int NOT NULL AUTO_INCREMENT,
   `tematica` varchar(120) NOT NULL,
   `estado` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
+-- 
 -- Dumping data for table `eje`
 --
 
@@ -92,13 +92,13 @@ LOCK TABLES `eje` WRITE;
 /*!40000 ALTER TABLE `eje` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
+-- 
 -- Table structure for table `eje_seleccionado`
 --
 
 DROP TABLE IF EXISTS `eje_seleccionado`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `eje_seleccionado` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_rendicion` int DEFAULT NULL,
@@ -109,10 +109,10 @@ CREATE TABLE `eje_seleccionado` (
   KEY `fk_es_eje` (`id_eje`),
   CONSTRAINT `fk_es_eje` FOREIGN KEY (`id_eje`) REFERENCES `eje` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_es_rendicion` FOREIGN KEY (`id_rendicion`) REFERENCES `rendicion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
+-- 
 -- Dumping data for table `eje_seleccionado`
 --
 
@@ -121,13 +121,13 @@ LOCK TABLES `eje_seleccionado` WRITE;
 /*!40000 ALTER TABLE `eje_seleccionado` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
+-- 
 -- Table structure for table `historial_admin`
 --
 
 DROP TABLE IF EXISTS `historial_admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `historial_admin` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_admin` int NOT NULL,
@@ -139,10 +139,10 @@ CREATE TABLE `historial_admin` (
   KEY `fk_ha_realizado` (`realizado_por`),
   CONSTRAINT `fk_ha_admin` FOREIGN KEY (`id_admin`) REFERENCES `administrador` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_ha_realizado` FOREIGN KEY (`realizado_por`) REFERENCES `administrador` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
+-- 
 -- Dumping data for table `historial_admin`
 --
 
@@ -151,13 +151,13 @@ LOCK TABLES `historial_admin` WRITE;
 /*!40000 ALTER TABLE `historial_admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
+-- 
 -- Table structure for table `pregunta`
 --
 
 DROP TABLE IF EXISTS `pregunta`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `pregunta` (
   `id` int NOT NULL AUTO_INCREMENT,
   `contenido` text NOT NULL,
@@ -168,10 +168,10 @@ CREATE TABLE `pregunta` (
   KEY `fk_p_eje` (`id_eje`),
   CONSTRAINT `fk_p_eje` FOREIGN KEY (`id_eje`) REFERENCES `eje` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_p_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
+-- 
 -- Dumping data for table `pregunta`
 --
 
@@ -180,13 +180,13 @@ LOCK TABLES `pregunta` WRITE;
 /*!40000 ALTER TABLE `pregunta` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
+-- 
 -- Table structure for table `pregunta_seleccionada`
 --
 
 DROP TABLE IF EXISTS `pregunta_seleccionada`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `pregunta_seleccionada` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_eje_seleccionado` int DEFAULT NULL,
@@ -196,10 +196,10 @@ CREATE TABLE `pregunta_seleccionada` (
   KEY `fk_ps_pregunta` (`id_pregunta`),
   CONSTRAINT `fk_ps_es` FOREIGN KEY (`id_eje_seleccionado`) REFERENCES `eje_seleccionado` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_ps_pregunta` FOREIGN KEY (`id_pregunta`) REFERENCES `pregunta` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
+-- 
 -- Dumping data for table `pregunta_seleccionada`
 --
 
@@ -208,23 +208,23 @@ LOCK TABLES `pregunta_seleccionada` WRITE;
 /*!40000 ALTER TABLE `pregunta_seleccionada` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
+-- 
 -- Table structure for table `rendicion`
 --
 
 DROP TABLE IF EXISTS `rendicion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `rendicion` (
   `id` int NOT NULL AUTO_INCREMENT,
   `fecha` date NOT NULL,
   `hora` time NOT NULL,
-  `banner` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `banner` varchar(255) COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
+-- 
 -- Dumping data for table `rendicion`
 --
 
@@ -233,13 +233,13 @@ LOCK TABLES `rendicion` WRITE;
 /*!40000 ALTER TABLE `rendicion` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
+-- 
 -- Table structure for table `usuario`
 --
 
 DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(60) NOT NULL,
@@ -254,10 +254,10 @@ CREATE TABLE `usuario` (
   PRIMARY KEY (`id`),
   KEY `fk_usuario_r` (`id_rendicion`),
   CONSTRAINT `fk_usuario_r` FOREIGN KEY (`id_rendicion`) REFERENCES `rendicion` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
+-- 
 -- Dumping data for table `usuario`
 --
 
