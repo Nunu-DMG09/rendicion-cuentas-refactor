caf44907c9f33dcba607f18546290803f95d038a
PetusoTwo <1566258@senati.pe>
2025-11-19 21:57:39 -0500

    Eliminar padding vertical en el contenedor principal del Layout

--- frontend/src/app/layouts/Layout.tsx
+++ frontend/src/app/layouts/Layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: Props) {
   return (
     <div className="min-h-screen flex flex-col bg-white text-gray-900">
       <Header />
-      <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2">
+      <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         {children}
       </main>
     </div>
