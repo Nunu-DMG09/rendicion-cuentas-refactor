d8437a8a63309ea9d465b74e44d508b87126cf02
PetusoTwo <1566258@senati.pe>
2025-11-19 21:50:49 -0500

    Ajustar clase de contenedor en el componente Header para mejorar el diseño

--- frontend/src/app/layouts/components/Header.tsx
+++ frontend/src/app/layouts/components/Header.tsx
@@ -3,7 +3,7 @@ import { IoAddCircleSharp } from "react-icons/io5";
 export default function Header() {
   return (
     <header className="bg-white border-b border-gray-200 p-5">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className=" mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center gap-3 cursor-pointer hover:scale-105 transition-all">
             <img src={logo} alt="Logo Municipalidad José Leonarlo Ortiz" className="h-20 w-auto" />
