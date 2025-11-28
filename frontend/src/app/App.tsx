9982405d08008d7f6e711f59a7c4e85b6937b62a
PetusoTwo <1566258@senati.pe>
2025-11-28 15:04:46 -0500

    feat: Add DashboardPage component and update admin routes

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,25 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Layout, AdminLayout } from './layouts'
+import { SidebarProvider } from './providers/SidebarProvider'
 import HomePage from './pages/HomePage'
 import RendicionPage from './pages/RendicionPage'
 import RegistrationPage from './pages/RegistrationPage'
-import { SidebarProvider } from './providers/SidebarProvider'
+import DashboardPage from '../features/dashboard/pages/DashboardPage'
 
 function App() {
   return (
     <Router>
       <SidebarProvider>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route path="/rendicion/:rendicionId" element={<RendicionPage />} />
-              <Route path="/register/:rendicionId" element={<RegistrationPage />} />
-            </Route>
-            <Route path='/admin' element={<AdminLayout />}>
-              <Route index element={<h1>hola:v</h1>} />
-              {/* Aqui van a renderizar todas las paginas que esten debajo de admin */}
-            </Route>
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/rendicion/:rendicionId" element={<RendicionPage />} />
+            <Route path="/register/:rendicionId" element={<RegistrationPage />} />
+          </Route>
+          <Route path='/admin' element={<AdminLayout />}>
+            <Route index element={<DashboardPage />} />
+            {/* Aqui van a renderizar todas las paginas que esten debajo de admin */}
+          </Route>
+        </Routes>
       </SidebarProvider>
     </Router>
   )
