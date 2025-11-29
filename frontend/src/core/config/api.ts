9cd590461b5ee82f4143bcb82c74e536cf5250d2
Diego17 <diegoalbertocastropastor@gmail.com>
2025-11-29 17:14:13 -0500

    feat: Implement API configuration with axios and response interceptors

---
+++ frontend/src/core/config/api.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { env } from "./env";
+
+export const api = axios.create({
+    baseURL: env.BACKEND_URL,
+    timeout: 10000,
+})
+
+api.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response?.data?.message) throw new Error(error.response.data.message)
+        if (error.code === "ECONNABORTED") throw new Error("La solicitud ha excedido el tiempo de espera.")
+        if (!error.response) throw new Error("Error de red. Por favor, verifica tu conexión a internet.")
+        throw new Error("Ha ocurrido un error inesperado.")
+    }
+)
