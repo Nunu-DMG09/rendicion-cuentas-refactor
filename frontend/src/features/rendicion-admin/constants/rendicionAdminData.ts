64dce1792bdbecaea578db47f6febafcef5a4a08
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-01 16:25:52 -0500

    feat: Update comments for image formats and available years constants

--- frontend/src/features/rendicion-admin/constants/rendicionAdminData.ts
+++ frontend/src/features/rendicion-admin/constants/rendicionAdminData.ts
@@ -1,3 +1,8 @@
+// Formatos de imagen aceptados
+export const ACCEPTED_IMAGE_FORMATS = '.jpg,.jpeg,.png,.webp,.gif'
+export const MAX_FILE_SIZE_MB = 5
+export const MAX_FILES = 10
+
 // Años disponibles
-// TODO: Obtener dinámicamente desde el backend
+// TODO: Esto debería venir de la API
 export const AVAILABLE_YEARS = [2026, 2025, 2024, 2023, 2022, 2021]
