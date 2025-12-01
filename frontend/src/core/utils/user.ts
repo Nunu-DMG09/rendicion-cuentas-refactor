fceb928af5261a6189c57254ef497f38009fa028
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-01 14:42:47 -0500

    feat: Add firstAdminName utility function to extract and capitalize the first name

--- frontend/src/core/utils/user.ts
+++ frontend/src/core/utils/user.ts
@@ -1,3 +1,8 @@
+import { capitalize } from "@/shared/utils";
+
 export const isSuperAdmin = (role: string): boolean => {
     return role === 'super_admin';
+}
+export const firstAdminName = (fullName: string): string => {
+    return capitalize(fullName.split(' ')[0]);
 }
