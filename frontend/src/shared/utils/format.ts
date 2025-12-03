377686f83b1fc1c4cd19a88bb0873010ad723a44
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-02 23:38:33 -0500

    feat: Add formatName utility function to format full names

--- frontend/src/shared/utils/format.ts
+++ frontend/src/shared/utils/format.ts
@@ -3,3 +3,9 @@ export const formatFileSize = (bytes: number) => {
 	if (bytes < 1024 * 1024) return (bytes / 1024).toFixed(1) + " KB";
 	return (bytes / (1024 * 1024)).toFixed(1) + " MB";
 };
+
+export const formatName = (
+	names: string,
+	paternalSurname: string,
+	maternalSurname: string
+) => `${paternalSurname} ${maternalSurname}, ${names}`;
