6115efd1105d82e34a6e0deb097a487a69828753
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-07 13:53:55 -0500

    feat: Add route for HistorialAdmin component to manage historial actions

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -21,6 +21,7 @@ import { ProtectedRoute } from "./routes/ProtectedRoute";
 import { SeleccionarPreguntas } from "@/features/preguntas/pages/SeleccionarPreguntas";
 import { NewUser } from "@/features/users/pages/NewUser";
 import { ListUsers } from "@/features/users/pages/ListUsers";
+import { HistorialAdmin } from "@/features/historial/pages/HistorialAdmin";
 
 function App() {
 	return (
@@ -116,6 +117,14 @@ function App() {
 								element={
 									<ProtectedRoute>
 										<ListUsers />
+									</ProtectedRoute>
+								}
+							/>
+							<Route
+								path="usuarios/historial-acciones"
+								element={
+									<ProtectedRoute>
+										<HistorialAdmin />
 									</ProtectedRoute>
 								}
 							/>
